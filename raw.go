20bb053dd60d6116abc16801cf4f0ae82818531e
wneo <wuyuan2012@gmail.com>
2018-05-04 20:06:51 +0800

    fix

--- raw.go
+++ raw.go
@@ -85,7 +85,7 @@ func (c *Conn) SetWriteDeadline(t time.Time) error {
 	return c.p.SetWriteDeadline(t)
 }
 
-var _ bpf.Setter = &Conn{}
+//var _ bpf.Setter = &Conn{}
 
 // SetBPF attaches an assembled BPF program to the connection.
 func (c *Conn) SetBPF(filter []bpf.RawInstruction) error {
